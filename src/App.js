1eaf7e77973856e9f928a901004c66a857ca96ee
jbuyu <jbuyu@ymail.com>
2021-04-28 19:09:43 +0300

    styles select, search bars

--- src/App.js
+++ src/App.js
@@ -1,41 +1,89 @@
-import { useState } from 'react';
-import './App.css';
-import FlashCardList from './Flash/FlashCardList';
+import Axios from "axios";
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+import FlashCardList from "./Flash/FlashCardList";
+const CATEGORY_API = "https://opentdb.com/api_category.php";
 
 function App() {
+  const categoryEl = useRef();
+  const amountEl = useRef();
+  const [flashCards, setFlashCards] = useState([]);
 
-  const [flashCards, setFlashCards] = useState(SAMPLE_FLASHCARDS)
+  const [categories, setCategories] = useState([]);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    Axios.get("https://opentdb.com/api.php", {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value,
+      },
+    }).then((res) => {
+      setFlashCards(
+        res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer,
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }
+  useEffect(() => {
+    Axios.get(CATEGORY_API).then((res) => {
+      setCategories(res.data.trivia_categories);
+    });
+  }, []);
+  useEffect(() => {}, []);
+
   return (
-    <div className="container">
-      <FlashCardList flashCards={flashCards}/>
-    </div>
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input
+            type="number"
+            id="amount"
+            min="1"
+            step="1"
+            defaultValue={10}
+            ref={amountEl}
+          />
+        </div>
+        <div className="form-group">
+          <div className="btn-class">
+            <button className="btn">Generate</button>
+          </div>
+        </div>
+      </form>
+      <div className="container">
+        <FlashCardList flashCards={flashCards} />
+      </div>
+    </>
   );
 }
-
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id:1,
-    question:'What is 2+2?',
-    answer: '4',
-    options:[
-      '2',
-      '3',
-      '4',
-      '1.5'
-    ]
-  },
-  {
-    id:2,
-    question:'Question 2?',
-    answer: 'Answer',
-    options:[
-      'Answer',
-      'Answer 1',
-      'Answer 2',
-      'Answer 3'
-    ]
-  }
-]
 
 export default App;
--- src/Flash/FlashCardList.js
+++ src/Flash/FlashCardList.js
@@ -1,33 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import FlashCard from "./FlashCard";
-import Axios from "axios";
-const BASE_URL = "https://opentdb.com/api.php?amount=10";
 
-export default function FlashCardList() {
-  const [flashCards, setFlashCards] = useState([]);
-
-  useEffect(() => {
-    Axios.get(BASE_URL).then((res) => {
-      setFlashCards(
-        res.data.results.map((questionItem, index) => {
-          const answer = decodeString(questionItem.correct_answer);
-          const options = [...questionItem.incorrect_answers.map(a=>decodeString(a)), answer];
-          return {
-            id: `${index}-${Date.now()}`,
-            question: decodeString(questionItem.question) ,
-            answer,
-            options: options.sort(() => Math.random() - 0.5),
-          };
-        })
-      );
-    });
-  }, []);
-
-  function decodeString(str){
-      const textArea = document.createElement('textarea')
-      textArea.innerHTML = str
-      return textArea.value;
-  }
+export default function FlashCardList({ flashCards }) {
   return (
     <div className="card-grid">
       {flashCards &&
